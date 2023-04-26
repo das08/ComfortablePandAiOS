904e0c1c8bf29b3d80b5f2bd0263acf5d4edd60b
das08 <41512077+das08@users.noreply.github.com>
2023-04-26 18:41:34 +0900

    init

---
+++ ComfortablePandAiOS/ComfortablePandAiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComfortablePandAiOSApp.swift
+//  ComfortablePandAiOS
+//
+//  Created by Kazuki Takeda on 2023/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ComfortablePandAiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
